447febaf74d29600f7cc18d7c299818a6c6db687
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-07-08 14:39:03 -0500

    feat: :sparkles: Visualizacion de POO, todas las clases, metodos y herencia ordenados

--- js/main.js
+++ js/main.js
@@ -10,11 +10,7 @@ class Persona {
     }
 
     static esMayorDeEdad(edad){
-        if(edad >= 18){
-            return 'True';
-        } else{
-            return 'False';
-        }
+        return edad >= 18;
     }
 }
 
@@ -24,13 +20,16 @@ const persona1 = new Persona("Guillermo", 19, "M");
 const mayorDeEdad = Persona.esMayorDeEdad(persona1.edad);
 console.log(`Es ${persona1.nombre} mayor de edad? ${mayorDeEdad}`);
 
-// document.querySelector("#saludo").innerHTML = /*html*/`
-//     <h1>Nombre: <span>${Persona.saludar(persona1)}</span></h1>
-    
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <h1>Persona:</h1>
+    <h5>Nombre: <span>${persona1.nombre}</span></h5>
+    <h5>Edad: <span>${persona1.edad}</span></h5>
+    <h5>Sexo: <span>${persona1.sexo}</span></h5>
+    <h5>Saludo: <span>${Persona.saludar(persona1)}</span></h5>
+    <h5>¿Es mayor de edad?: <span>${mayorDeEdad}</span></h5>
+`;
 
 class Estudiante extends Persona{
-    carrera;
     constructor(nombre, edad, sexo, carrera){
         super(nombre, edad, sexo);
         this.carrera = carrera;
@@ -42,13 +41,16 @@ class Estudiante extends Persona{
 }
 
 // Instancia de la clase estudiante
-const estudiante1 = new Estudiante("Guillermo", 19, "M", "Desarrollador de Software")
+const estudiante1 = new Estudiante("Guillermo", 19, "M", "Desarrollador de Software");
 
-// document.querySelector("#saludo").innerHTML = /*html*/`
-//     <h1>Nombre: <span>${Persona.saludar(estudiante1)}</span></h1>
-//     <h2>Carrera: <span>${Estudiante.estudiar(estudiante1)}</span></h2>
-//     <h3>Mayor de Edad: <span>${Persona.esMayorDeEdad(persona1.edad)}</span></h3>
-//     `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Estudiante:</h1>
+    <h5>Nombre: <span>${estudiante1.nombre}</span></h5>
+    <h5>Edad: <span>${estudiante1.edad}</span></h5>
+    <h5>Sexo: <span>${estudiante1.sexo}</span></h5>
+    <h5>Carrera: <span>${Estudiante.estudiar(estudiante1)}</span></h5>
+`;
 
 class Animal {
     constructor(nombre, edad){
@@ -57,37 +59,43 @@ class Animal {
     }
 
     static hacerSonido(sonido){
-        return `Guau Guau`;
+        return sonido;
     }
 }
 
 // Crear una instancia para Animal
 const animal1 = new Animal("Doky", 14);
 
-// document.querySelector("#saludo").innerHTML = /*html*/ `
-//     <h4>Nombre: <span>${animal1.nombre}</span></h4>
-//     <h4>Sonido: <span>${Animal.hacerSonido(animal1, "Guau Guau")}</span></h4>
-//     <h4>Estado: <span>${Animal.moverCola(animal1)}</span></h4>
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Animal:</h1>
+    <h5>Nombre: <span>${animal1.nombre}</span></h5>
+    <h5>Edad: <span>${animal1.edad}</span></h5>
+    <h5>Sonido: <span>${Animal.hacerSonido("Guau Guau")}</span></h5>
+`;
 
 class Perro extends Animal{
-    raza;
     constructor(nombre, edad, raza){
         super(nombre, edad);
         this.raza = raza;
     }
 
     static moverCola(perro){
-        return `El perro esta moviendo la cola`;
+        return `El perro ${perro.nombre} está moviendo la cola`;
     }
 }
 
-// document.querySelector("#saludo").innerHTML = /*html*/ `
-//     <h4>Nombre: <span>${animal1.nombre}</span></h4>
-//     <h4>Sonido: <span>${Animal.hacerSonido(animal1)}</span></h4>
-//     <h4>Estado: <span>${Perro.moverCola(animal1)}</span></h4>
-// `;
+// Instancia para Perro
+const perro1 = new Perro("Doky", 14, "Clasific");
 
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Perro:</h1>
+    <h5>Nombre: <span>${perro1.nombre}</span></h5>
+    <h5>Edad: <span>${perro1.edad}</span></h5>
+    <h5>Raza: <span>${perro1.raza}</span></h5>
+    <h5>Estado: <span>${Perro.moverCola(perro1)}</span></h5>
+`;
 
 class Figura {
     constructor(color, area){
@@ -96,43 +104,43 @@ class Figura {
     }
 
     static calcularArea(figura){
-        return `Area: ${figura.area}`;
+        return `Área: ${figura.area}`;
     }
 }
 
-//Crear una instancia para mi figurea
-const figura1 = new Figura ("Azul", 24);
+// Crear una instancia para Figura
+const figura1 = new Figura("Azul", 24);
 
-// document.querySelector("#saludo").innerHTML = /*html*/ `
-//     <h5>Figura: <span>${figura1.color}</span></h5>
-//     <h5><span>${Figura.calcularArea(figura1)}</span></h5>
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Figura:</h1>
+    <h5>Color: <span>${figura1.color}</span></h5>
+    <h5>Área: <span>${Figura.calcularArea(figura1)}</span></h5>
+`;
 
 class Circulo extends Figura {
-    radio;
     constructor(color, area, radio) {
         super(color, area);
         this.radio = radio;
     }
 
     static calcularArea(circulo){
-        const area = Math.PI * circulo.radio **2;
-        return `Area del Circulo: ${area.toFixed(2)}`;
+        const area = Math.PI * circulo.radio ** 2;
+        return `Área del Círculo: ${area.toFixed(2)}`;
     }
 }
 
-//Instancia para circulo
-const circulo1 = new Circulo ("Yellow",0, 8);
+// Instancia para Círculo
+const circulo1 = new Circulo("Yellow", 0, 8);
 
-// document.querySelector("#saludo").innerHTML = /*html*/ `
-//     <h5>Figura: <span>${figura1.color}</span></h5>
-//     <h5><span>${Figura.calcularArea(figura1)}</span></h5>
-//     <h5><span>${Circulo.calcularArea(circulo1)}</span></h5>
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Círculo:</h1>
+    <h5>Color: <span>${circulo1.color}</span></h5>
+    <h5>Área: <span>${Circulo.calcularArea(circulo1)}</span></h5>
+`;
 
 class Rectangulo extends Figura {
-    largo;
-    ancho;
     constructor(color, area, largo, ancho) {
         super(color, area);
         this.largo = largo;
@@ -145,14 +153,15 @@ class Rectangulo extends Figura {
     }
 }
 
-//Instancia para Rectangulo
-const rectangulo1 = new Rectangulo ("Green", null, 7, 3);
+// Instancia para Rectángulo
+const rectangulo1 = new Rectangulo("Green", null, 7, 3);
 
-// document.querySelector("#saludo").innerHTML = /*html*/ `
-//     <h5>Figura: <span>${figura1.color}</span></h5>
-//     <h5><span>${Figura.calcularArea(figura1)}</span></h5>
-//     <h5><span>${Rectangulo.calcularArea(rectangulo1)}</span></h5>
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Rectángulo:</h1>
+    <h5>Color: <span>${rectangulo1.color}</span></h5>
+    <h5>Área: <span>${Rectangulo.calcularArea(rectangulo1)}</span></h5>
+`;
 
 class Vehiculo {
     constructor(marca, modelo, velocidad) {
@@ -167,14 +176,21 @@ class Vehiculo {
     }
 
     static convertirKmHEnMph(velocidadKmH) {
-        // Divide la velocidad en km por hora entre 1.60934
         return velocidadKmH / 1.60934;
     }
 }
 
-// Crear una instancia para vehiculo
+// Crear una instancia para Vehiculo
 const vehiculo1 = new Vehiculo("Chevrolet", "Camaro Z-10", 150);
 vehiculo1.acelerar();
+
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Vehículo:</h1>
+    <h5>Marca: <span>${vehiculo1.marca}</span></h5>
+    <h5>Modelo: <span>${vehiculo1.modelo}</span></h5>
+    <h5>Velocidad: <span>${vehiculo1.velocidad} km/h</span></h5>
+`;
 
 class Coche extends Vehiculo {
     constructor(marca, modelo, velocidad, combustible) {
@@ -188,40 +204,40 @@ class Coche extends Vehiculo {
     }
 }
 
-// Instancia para coche
+// Instancia para Coche
 const coche1 = new Coche("Lamborghini", "Huracan", 250, "Gasolina");
 coche1.acelerar();
 
-// Convertir la velocidad del coche a mph
+// Convertir la velocidad del Coche a mph
 const velocidadMph = Vehiculo.convertirKmHEnMph(coche1.velocidad).toFixed(2);
 
-// document.querySelector("#saludo").innerHTML = `
-//     <h5>Marca del Vehiculo: <span>${coche1.marca}</span></h5>
-//     <h5>Modelo: <span>${coche1.modelo}</span></h5>
-//     <h5>Velocidad: <span>${coche1.velocidad} km/h</span></h5>
-//     <h5>Velocidad en Mph: <span>${velocidadMph} mph</span></h5>
-//     <h5>Combustible Disponible: <span>${coche1.combustible}</span></h5>
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Coche:</h1>
+    <h5>Marca: <span>${coche1.marca}</span></h5>
+    <h5>Modelo: <span>${coche1.modelo}</span></h5>
+    <h5>Velocidad: <span>${coche1.velocidad} km/h</span></h5>
+    <h5>Velocidad en Mph: <span>${velocidadMph} mph</span></h5>
+    <h5>Combustible: <span>${coche1.combustible}</span></h5>
+`;
 
 class Empleado {
     constructor(nombre, edad, sueldo){
         this.nombre = nombre;
         this.edad = edad;
         this.sueldo = sueldo;
-        this.id = Empleado.generaldEmpleado();
+        this.id = Empleado.generarIdUnico();
     }
 
     calcularSalarioAnual(){
-        // Retornar salario anual del empleado (multiplicamos el sueldo mensual por 12)
         return this.sueldo * 12;
     }
 
-    static generaldEmpleado(){
-        //retorna un id unico para cada empleado, usar var estatica par incrementar el id cada vez q se cree una instancia de empleado
+    static generarIdUnico(){
         if (!this.proximoId) {
-            this.proximoId = 1; // Inicializar el contador estático si no existe
+            this.proximoId = 1;
         } else {
-            this.proximoId++; // Incrementar el contador estático
+            this.proximoId++;
         }
         return this.proximoId;
     }
@@ -231,11 +247,14 @@ class Empleado {
 const empleado1 = new Empleado("Albert", 24, 1200000);
 const salarioAnual = empleado1.calcularSalarioAnual();
 
-// document.querySelector("#saludo").innerHTML = `
-//     <h5>Nombre: <span>${empleado1.nombre}</span></h5>
-//     <h5>Edad: <span>${empleado1.edad}</span></h5>
-//     <h5>Salario Anual: <span>${salarioAnual}</span></h5>
-// `;
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Empleado:</h1>
+    <h5>Nombre: <span>${empleado1.nombre}</span></h5>
+    <h5>Edad: <span>${empleado1.edad}</span></h5>
+    <h5>Salario Anual: <span>${salarioAnual}</span></h5>
+    <h5>ID: <span>${empleado1.id}</span></h5>
+`;
 
 class Gerente extends Empleado {
     constructor(nombre, edad, sueldo, departamento){
@@ -244,24 +263,24 @@ class Gerente extends Empleado {
     }
 
     calcularSalarioAnualConBono(){
-        // Calcular salario anual con bono del 10%
         const salarioConBono = this.sueldo * 1.1;
         const salarioAnual = salarioConBono * 12;
-
-        // Formatear el salario anual para que sea más entendible
-        const salariototal = salarioAnual.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        const salarioFormateado = salarioAnual.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
         
-        return salariototal;
+        return salarioFormateado;
     }
 }
+
 // Instancia para Gerente con salario inicial de 1400000
 const gerente1 = new Gerente("Alejo", 26, 1400000, "Ventas");
 const salarioAnualGerenteConBono = gerente1.calcularSalarioAnualConBono();
 
-document.querySelector("#saludo").innerHTML = `
+document.querySelector("#saludo").innerHTML += /*html*/ `
+    <hr>
+    <h1>Gerente:</h1>
     <h5>Nombre: <span>${gerente1.nombre}</span></h5>
     <h5>Edad: <span>${gerente1.edad}</span></h5>
     <h5>Salario Anual con Bono: <span>${salarioAnualGerenteConBono} COP</span></h5>
     <h5>Departamento: <span>${gerente1.departamento}</span></h5>
-    <h5>Id: <span>${gerente1.id}</span></h5>
+    <h5>ID: <span>${gerente1.id}</span></h5>
 `;
