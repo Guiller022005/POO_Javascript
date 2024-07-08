ce8d90dd151e4a135eab7e24b53669ad0d26024d
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-07-08 08:12:36 -0500

    feat: :sparkles: Creacion de la clase Animal, metodo hacer sonido y instancia animal1, clase perro hereda de animal

--- js/main.js
+++ js/main.js
@@ -44,8 +44,46 @@ class Estudiante extends Persona{
 // Instancia de la clase estudiante
 const estudiante1 = new Estudiante("Guillermo", 19, "M", "Desarrollador de Software")
 
-document.querySelector("#saludo").innerHTML = /*html*/`
-    <h1>Nombre: <span>${Persona.saludar(estudiante1)}</span></h1>
-    <h2>Carrera: <span>${Estudiante.estudiar(estudiante1)}</span></h2>
-    <h3>Mayor de Edad: <span>${Persona.esMayorDeEdad(persona1.edad)}</span></h3>
-    `;
+// document.querySelector("#saludo").innerHTML = /*html*/`
+//     <h1>Nombre: <span>${Persona.saludar(estudiante1)}</span></h1>
+//     <h2>Carrera: <span>${Estudiante.estudiar(estudiante1)}</span></h2>
+//     <h3>Mayor de Edad: <span>${Persona.esMayorDeEdad(persona1.edad)}</span></h3>
+//     `;
+
+class Animal {
+    constructor(nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    static hacerSonido(sonido){
+        return `Guau Guau`;
+    }
+}
+
+// Crear una instancia para Animal
+const animal1 = new Animal("Doky", 14);
+
+// document.querySelector("#saludo").innerHTML = /*html*/ `
+//     <h4>Nombre: <span>${animal1.nombre}</span></h4>
+//     <h4>Sonido: <span>${Animal.hacerSonido(animal1, "Guau Guau")}</span></h4>
+//     <h4>Estado: <span>${Animal.moverCola(animal1)}</span></h4>
+// `;
+
+class Perro extends Animal{
+    raza;
+    constructor(nombre, edad, raza){
+        super(nombre, edad);
+        this.raza = raza;
+    }
+
+    static moverCola(perro){
+        return `El perro esta moviendo la cola`;
+    }
+}
+
+document.querySelector("#saludo").innerHTML = /*html*/ `
+    <h4>Nombre: <span>${animal1.nombre}</span></h4>
+    <h4>Sonido: <span>${Animal.hacerSonido(animal1)}</span></h4>
+    <h4>Estado: <span>${Perro.moverCola(animal1)}</span></h4>
+`;
